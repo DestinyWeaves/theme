280ec8a733ddba887c708ba8bbb10e88d0629193
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-04 19:44:42 -0400

    put assets in subfolder

--- flat_assets.sh
+++ flat_assets.sh
@@ -3,21 +3,19 @@
 set -x
 shopt -s globstar extglob
 
-replace_token="$1"
-site_path="$2"
-flat_path="$3"
-url_prefix="$4"
+site_path="$1"
+url_prefix="$2"
 
-mkdir -p "${flat_path}"
+mkdir -p "${site_path}/flat"
 
 for filename in ${site_path}/assets/**/*.@(js|css|jpg|png); do
     orig_name="${filename/#$site_path\/assets\/}"
     flat_name="${orig_name//\//__}"
 
     # copy to the flattened asset location
-    cp "${filename}" "${flat_path}/${flat_name}"
+    cp "${filename}" "${site_path}/flat/${flat_name}"
 
-    rlink_name="/${replace_token}/assets/${orig_name}"
+    rlink_name="/assets/${orig_name}"
     asset_name="${url_prefix}/${flat_name}"
 
     # replace the generated links with links to the flattened asset location
