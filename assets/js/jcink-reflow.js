4598bf054613165fd63f9ad1a8d33b755d2ee0ea
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-05 19:28:47 -0400

    use defer script mode

--- assets/js/jcink-reflow.js
+++ assets/js/jcink-reflow.js
@@ -1,16 +1,13 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll(".jcink-reflow-breadcrumbs").forEach(nav => {
-    const strip = nav.querySelector("#navstrip");
-    const links = strip.querySelectorAll("a");
-    const navlist = document.createElement("ol");
-    navlist.className="breadcrumb-nav-list";
-    links.forEach(link => {
-      const navitem = document.createElement("li");
-      navitem.className="breadcrumb-nav-list-item";
-      navitem.appendChild(link);
-      navlist.appendChild(navitem);
-    });
-    nav.replaceChildren(navlist);
+
+document.querySelectorAll(".jcink-reflow-breadcrumbs").forEach(nav => {
+  const navlist = document.createElement("ol");
+  navlist.className="breadcrumb-nav-list";
+  nav.querySelector("#navstrip").querySelectorAll("a").forEach(link => {
+    const navitem = document.createElement("li");
+    navitem.className="breadcrumb-nav-list-item";
+    navitem.appendChild(link);
+    navlist.appendChild(navitem);
   });
+  nav.replaceChildren(navlist);
 });
