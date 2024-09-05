8f309f7edeb496971de3c3c3b4451568bed615a3
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-04 21:16:29 -0400

    local build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+asset_url_prefix="https://files.jcink.net/uploads2/ajmansfieldtestboard/assets"
+theme_name=DestinyWeaves
+theme_variant=dark
+
+bundle exec jekyll build
+
+./flat_assets.sh _site "${asset_url_prefix}"
+
+cybertron build \
+  --name "${theme_name}-${theme_variant}" \
+  --macros-folder "_site/macros" \
+  --stylesheet "_site/assets/css/just-the-docs-${theme_variant}.css" \
+  --templates-folder "_site/html-templates" \
+  --wrapper "_site/wrapper.html"
+
+    
+
