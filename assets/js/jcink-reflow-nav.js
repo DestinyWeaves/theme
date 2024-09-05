a1f58e711bc32716d687d6d858b301c17c0f184f
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-05 17:44:24 -0400

    use class to reflow elements

--- assets/js/jcink-reflow-nav.js
+++ assets/js/jcink-reflow-nav.js
@@ -1,13 +1,17 @@
-function jcink_reflow_nav(nav){
-  const strip = nav.querySelector("#navstrip");
-  const links = strip.querySelectorAll("a");
-  const navlist = document.createElement("ol");
-  navlist.className="breadcrumb-nav-list";
-  links.forEach(link => {
-    const navitem = document.createElement("li");
-    navitem.className="breadcrumb-nav-list-item";
-    navitem.appendChild(link);
-    navlist.appendChild(navitem);
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll(".jcink-reflow-nav").forEach(nav => {
+    const strip = nav.querySelector("#navstrip");
+    const links = strip.querySelectorAll("a");
+    const navlist = document.createElement("ol");
+    navlist.className="breadcrumb-nav-list";
+    links.forEach(link => {
+      const navitem = document.createElement("li");
+      navitem.className="breadcrumb-nav-list-item";
+      navitem.appendChild(link);
+      navlist.appendChild(navitem);
+    });
+    nav.replaceChildren(navlist);
+    nav.classList.remove(".jcink-reflow-nav");
   });
-  nav.replaceChildren(navlist);
-}
+});
