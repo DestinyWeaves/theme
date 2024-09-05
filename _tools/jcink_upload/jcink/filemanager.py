fd9cdd6728dca1ee24f827a48d5e87a1c23080ff
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-05 15:46:35 -0400

    upload nonflat asset set

--- _tools/jcink_upload/jcink/filemanager.py
+++ _tools/jcink_upload/jcink/filemanager.py
@@ -1,10 +1,13 @@
+import logging
+log = logging.getLogger("fm")
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import InvalidSelectorException, NoSuchElementException
+from selenium.common.exceptions import InvalidSelectorException, NoSuchElementException, StaleElementReferenceException
 import os
 import errno
 from .frame import frame_selected
 from .adminpanel import AdminPanel
+
 
 class FileManager:
     def __init__(self, ap: AdminPanel):
@@ -13,35 +16,56 @@ class FileManager:
 
     def cd_single(self, component:str):
         if component == "": # i.e. begin component was /
+            log.info("nav to file manager page")
             self.ap.nav_to_section("File Manager")
         else:
             with frame_selected(self.driver, self.ap.body_frame):
-                table = self.driver.find_element(By.CSS_SELECTOR, "#acp_form .tableborder")
-                try:
-                    table.find_element(By.LINK_TEXT, component).click()
-                except InvalidSelectorException as e:
-                    raise FileNotFoundError(component) from e
+                log.info("nav to %s", component)
+                while True:
+                    table = self.driver.find_element(By.CSS_SELECTOR, "#acp_form .tableborder")
+                    try:
+                        table.find_element(By.LINK_TEXT, component).click()
+                    except StaleElementReferenceException:
+                        continue
+                    except InvalidSelectorException as e:
+                        raise FileNotFoundError(component) from e
+                    break
 
     def cd(self, path:str):
-        components = path.split("/")
+        log.info("cd %s", path)
+        components = path.split('/')
         for component in components:
             self.cd_single(component)
     
     def mkdir(self, path:str, parents=False):
         if parents:
+            log.info("mkdir -p %s", path)
             self.mkdir_parents(path)
+        elif path == "":
+            pass
+        else:
+            log.info("mkdir %s", path)
+            dirname, name = path.rsplit('/', 1)
+            self.cd(dirname)
 
-        dirname, name = path.rsplit('/', 1)
-        self.cd(dirname)
+            log.info("create dir %s", name)
+            # breakpoint()
 
-        with frame_selected(self.driver, self.ap.body_frame):
-            self.driver.find_element(By.NAME, "folder_name").send_keys(dirname)
-            self.driver.find_element(By.CSS_SELECTOR, "#button").click()
+            with frame_selected(self.driver, self.ap.body_frame):
+                while True:
+                    form = self.driver.find_element(By.NAME, "uploadform")
+                    try:
+                        form.find_element(By.NAME, "folder_name").send_keys(name)
+                    except StaleElementReferenceException:
+                        continue
+                    break
+                form.find_element(By.CSS_SELECTOR, "#button").click()
 
-            result_text = self.driver.find_element(By.CSS_SELECTOR, ".tdrow1").text
+                result_text = self.driver.find_element(By.CSS_SELECTOR, ".tdrow1").text
+            log.info("result %s", result_text)
 
-        if "already exists" in result_text:
-            raise FileExistsError(result_text)
+            if "already exists" in result_text:
+                raise FileExistsError(result_text)
         
     def mkdir_parents(self, path:str):
         components = path.split("/")
@@ -54,6 +78,7 @@ class FileManager:
                 pass
     
     def rm(self, path:str, file=True, dir=True):
+        log.info("rm %s", path)
         if file and dir:
             checkbox_query = (By.CSS_SELECTOR, "input[type=checkbox]")
         elif file and not dir:
@@ -67,11 +92,15 @@ class FileManager:
         self.cd(dirname)
 
         with frame_selected(self.driver, self.ap.body_frame):
-            table = self.driver.find_element(By.CSS_SELECTOR, "#acp_form .tableborder")
-            try:
-                label = table.find_element(By.LINK_TEXT, name) # <a> element
-            except NoSuchElementException as e:
-                raise FileNotFoundError(path) from e
+            while True:
+                table = self.driver.find_element(By.CSS_SELECTOR, "#acp_form .tableborder")
+                try:
+                    label = table.find_element(By.LINK_TEXT, name) # <a> element
+                except StaleElementReferenceException:
+                    continue
+                except NoSuchElementException as e:
+                    raise FileNotFoundError(path) from e
+                break
             row = label.find_element(By.XPATH,"../..") 
             checkbox = row.find_element(*checkbox_query)
             checkbox.click()
@@ -80,20 +109,28 @@ class FileManager:
             del_button.click()
 
             result_text = self.driver.find_element(By.CSS_SELECTOR, "#description").text
-            if result_text != "The action was executed successfully":
-                raise Exception(result_text)
+        log.info("result %s", result_text)
+        if result_text != "The action was executed successfully":
+            raise Exception(result_text)
     
     def upload_file(self, srcpath:str, dstdir:str) -> str:
+        log.info("upload %s -> %s", srcpath, dstdir)
         
-        srcdir, srcname = srcpath.rsplit('/', 1)
+        srcdir, srcname = os.path.split(srcpath)
 
         self.cd(dstdir)
         with frame_selected(self.driver, self.ap.body_frame):
-            form = self.driver.find_element(By.NAME, "uploadform")
-            form.find_element(By.NAME, "FILE_UPLOAD[]").send_keys(os.path.abspath(srcpath))
+            while True:
+                form = self.driver.find_element(By.NAME, "uploadform")
+                try:
+                    form.find_element(By.NAME, "FILE_UPLOAD[]").send_keys(os.path.abspath(srcpath))
+                except StaleElementReferenceException:
+                    continue
+                break
             form.find_element(By.CSS_SELECTOR, "#button").click()
 
             result_text = self.driver.find_element(By.CSS_SELECTOR, ".tdrow1").text
+            log.info("result %s", result_text)
 
             if "Files Uploaded" in result_text:
                 pass # success
@@ -104,18 +141,19 @@ class FileManager:
             else:
                 raise Exception(result_text)
         
-            table = self.driver.find_element(By.CSS_SELECTOR, "#acp_form .tableborder")
-            # breakpoint()
-            try:
-                labels = table.find_elements(By.LINK_TEXT, srcname) # <a> element
-            except NoSuchElementException as e:
-                raise FileNotFoundError(srcpath) from e
-            rows = [label.find_element(By.XPATH, "../..") for label in labels]
-            urls = [row.find_element(By.NAME, "copy").get_attribute("value") for row in rows]
+            # table = self.driver.find_element(By.CSS_SELECTOR, "#acp_form .tableborder")
+            # try:
+            #     labels = table.find_elements(By.LINK_TEXT, srcname) # <a> element
+            # except NoSuchElementException as e:
+            #     raise FileNotFoundError(srcpath) from e
+            # except:
+            #     breakpoint()
+            # rows = [label.find_element(By.XPATH, "../..") for label in labels]
+            # urls = [row.find_element(By.NAME, "copy").get_attribute("value") for row in rows]
             
-            if len(urls) != 1:
-                raise FileExistsError(f"duplicated files: {urls}")
-            return urls[0]
+            # if len(urls) != 1:
+            #     raise FileExistsError(f"duplicated files: {urls}")
+            # return urls[0]
 
 
 
