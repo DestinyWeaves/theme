0d8bf203bed6c5f80e50f43ab7c17f0e3000ae0c
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-05 15:46:58 -0400

    upload assets

--- _tools/jcink_upload/main.py
+++ _tools/jcink_upload/main.py
@@ -1,17 +1,56 @@
+import argparse
+import logging
+logging.basicConfig(level = logging.INFO)
+log = logging.getLogger("")
+
+import os
 from selenium import webdriver
 from jcink.adminpanel import AdminPanel
 from jcink.filemanager import FileManager
 
-admin_url="https://ajmansfieldtestboard.jcink.net/admin.php"
-username="github_actions"
-from getpass import getpass
-password = getpass()
+def environ_or_required(key):
+    return (
+        {'default': os.environ.get(key)} if os.environ.get(key)
+        else {'required': True}
+    )
+parser = argparse.ArgumentParser(
+    prog="JCINK Uploader",
+    description="Automate updating a skinpack through the JCINK Admin Panel",
+)
+parser.add_argument('--skin', action='append')
+parser.add_argument('--assets')
+parser.add_argument('--assets-root', default=os.curdir)
+parser.add_argument('--admin-url', **environ_or_required('JCINK_ADMINURL'))
+parser.add_argument('--admin-user', **environ_or_required('JCINK_USERNAME'))
+parser.add_argument('--admin-pass', **environ_or_required('JCINK_PASSWORD'))
+args = parser.parse_args()
 
-driver = webdriver.Chrome()
-driver.set_page_load_timeout(1)
-ap = AdminPanel(driver, admin_url)
-sess = ap.login(username, password).__enter__()
+
+driver = webdriver.Firefox()
+# driver.set_page_load_timeout(10)
+driver.implicitly_wait(1)
+ap = AdminPanel(driver, args.admin_url)
 fm = FileManager(ap)
-    
 
-    
+def fully_split_path(winpath:str):
+    components = []
+    while winpath:
+        winpath, component = os.path.split(winpath)
+        components.insert(0, component)
+    return components
+
+with ap.login(args.admin_user, args.admin_pass):
+    for root, dirs, files in os.walk(args.assets):
+        src_root = os.path.abspath(root)
+        dst_root = "/" + "/".join(fully_split_path(os.path.relpath(root, args.assets_root)))
+
+        log.info("mkdir %s -> %s", src_root, dst_root)
+        try:
+            fm.mkdir(dst_root)
+        except FileExistsError:
+            log.warning("folder already exists")
+        for file in files:
+            src_file = os.path.join(src_root, file)
+            dst_file = "/".join((dst_root, file))
+            log.info("upload %s -> %s", src_file, dst_file)
+            fm.upload_file(src_file, dst_root)
