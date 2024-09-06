fdd9aad47de416c7a981cde32c1a88542ad33148
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-06 13:52:07 -0400

    add deploy step to manual build script

--- _tools/build.sh
+++ _tools/build.sh
@@ -6,38 +6,33 @@ set -e -x
 
 asset_url_prefix="https://files.jcink.net/uploads2/ajmansfieldtestboard"
 theme_name=just-the-docs
-theme_variant=dark
 
 bundle exec jekyll build
 
 "$SCRIPT_DIR/flat_assets.sh" _site "${asset_url_prefix}" $(git describe --always --dirty --broken --tags)
 
-cybertron build \
-  --name "${theme_name} ${theme_variant} $(git describe --always --dirty --broken --tags)" \
-  --stylesheet "_site/assets/css/just-the-docs-${theme_variant}.css" \
-  --wrapper "_site/wrapper.html" \
-  --templates-folder "_site/html-templates/" \
-  --macros-folder "_site/macros/" \
-  --output-directory "_site/"
+for theme_variant in default dark light;
+do
 
-# server_path=../ajmansfieldtestboard
-# artifact_path=$server_path/artifact/
-# cache_path=$server_path/cache/
-# artifact_server_addr=192.168.65.254
-# # server_addr=172.17.0.2
-# cache_server_addr=host.docker.internal
+  cybertron build \
+    --name "${theme_name} ${theme_variant} $(git describe --always --dirty --broken --tags)" \
+    --stylesheet "_site/assets/css/just-the-docs-${theme_variant}.css" \
+    --wrapper "_site/wrapper.html" \
+    --templates-folder "_site/html-templates/" \
+    --macros-folder "_site/macros/" \
+    --output-directory "_site/"
 
+done
 
-# mkdir -p $artifact_path
-# mkdir -p $cache_path
+# need to set environment variables:
+# - JCINK_ADMINURL
+# - JCINK_USERNAME
+# - JCINK_PASSWORD
 
-# act \
-#   --env-file $server_path/.env \
-#   --var-file $server_path/.variables \
-#   --secret-file $server_path/.secrets \
-#   --artifact-server-addr $artifact_server_addr \
-#   --artifact-server-path $artifact_path \
-#   --cache-server-addr $cache_server_addr \
-#   --cache-server-path $cache_path \
-#   $@
-
+python "$SCRIPT_DIR/jcink_upload/main.py" \
+  --assets _site/files/ \
+  --assets-root _site/ \
+  --skin _site/*default*.xml \
+  --skin _site/*dark*.xml \
+  --skin _site/*light*.xml \
+  --upgrade-regex '^(?P<skin>just-the-docs) (?P<variant>dark|light|default) (?P<version>.*)$' \
