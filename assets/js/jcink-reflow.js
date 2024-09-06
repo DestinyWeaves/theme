a22a206cc581330044999a3820cb4d142596797e
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-05 21:16:24 -0400

    reflow the user login infobox

--- assets/js/jcink-reflow.js
+++ assets/js/jcink-reflow.js
@@ -24,5 +24,17 @@ document.querySelectorAll(".jcink-reflow-aux-nav").forEach(div => {
 });
 
 document.querySelectorAll(".jcink-reflow-user-sidebar").forEach(div => {
+  const table_cells = div.querySelector("#userlinks").querySelectorAll("td");
+  const login_info = table_cells[0];
+  const controls = table_cells[1];
+
+  // TODO: how does the login info look when a user is logged out?
+  const new_login_info = document.createElement("dl");
+  new_login_info.appendChild(document.createElement("dt").appendChild(login_info.querySelector("strong")));
+  new_login_info.appendChild(document.createElement("dl").appendChild(login_info.querySelector("#log-out")));
+  new_login_info.appendChild(document.createElement("dl").appendChild(login_info.querySelector("#admin-link")));
+  new_login_info.appendChild(document.createElement("dl").appendChild(login_info.querySelector("#modcp-link")));
+  div.insertAdjacentElement('beforebegin', new_login_info);
+  login_info.remove();
 
 });
