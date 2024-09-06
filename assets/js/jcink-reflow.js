38a2385019f56237a360f224b480d5d81ec6003e
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-05 21:38:34 -0400

    login nav styling

--- assets/js/jcink-reflow.js
+++ assets/js/jcink-reflow.js
@@ -2,10 +2,10 @@
 
 document.querySelectorAll(".jcink-reflow-breadcrumbs").forEach(nav => {
   const navlist = document.createElement("ol");
-  navlist.className="breadcrumb-nav-list";
+  navlist.className = "breadcrumb-nav-list";
   nav.querySelector("#navstrip").querySelectorAll("a").forEach(link => {
     const navitem = document.createElement("li");
-    navitem.className="breadcrumb-nav-list-item";
+    navitem.className = "breadcrumb-nav-list-item";
     navitem.appendChild(link);
     navlist.appendChild(navitem);
   });
@@ -15,31 +15,34 @@ document.querySelectorAll(".jcink-reflow-breadcrumbs").forEach(nav => {
 document.querySelectorAll(".jcink-reflow-aux-nav").forEach(div => {
   div.querySelector("#submenu").querySelectorAll("a").forEach(link => {
     const navitem = document.createElement("li");
-    navitem.className="aux-nav-list-item";
+    navitem.className = "aux-nav-list-item";
     navitem.appendChild(link);
-    link.className="site-button";
+    link.className = "site-button";
     div.insertAdjacentElement('beforebegin', navitem);
   })
   div.remove();
 });
 
-function mk_elem_containing(tag, content) {
-  const elem = document.createElement(tag);
-  elem.appendChild(content);
-  return elem
-}
 document.querySelectorAll(".jcink-reflow-user-sidebar").forEach(div => {
   const table_cells = div.querySelector("#userlinks").querySelectorAll("td");
-  const login_info = table_cells[0];
+  const linfo = table_cells[0];
   const controls = table_cells[1];
 
   // TODO: how does the login info look when a user is logged out?
-  const new_login_info = document.createElement("dl");
-  new_login_info.appendChild(mk_elem_containing("dt", login_info.querySelector("strong")));
-  new_login_info.appendChild(mk_elem_containing("dd", login_info.querySelector("#log-out")));
-  new_login_info.appendChild(mk_elem_containing("dd", login_info.querySelector("#admin-link")));
-  new_login_info.appendChild(mk_elem_containing("dd", login_info.querySelector("#modcp-link")));
-  div.insertAdjacentElement('beforebegin', new_login_info);
-  login_info.remove();
-
+  
+  const ninfo = document.createElement("div");
+  ninfo.className = "login-nav";
+  const ninfo_head = document.createElement("div");
+  ninfo_head.className = "login-nav-head";
+  ninfo.appendChild(ninfo_head);
+  const ninfo_list = document.createElement("ul");
+  ninfo_list.className = "login-nav-list";
+  for (let query in ["#log-out", "#admin-link", "#modcp-link"]) {
+    const list_link = linfo.querySelector(query);
+    const list_item = document.createElement("li");
+    list_item.className = "login-nav-list-item";
+    ninfo_list.appendChild(list_item);
+  }
+  div.insertAdjacentElement('beforebegin', ninfo);
+  // div.remove();
 });
