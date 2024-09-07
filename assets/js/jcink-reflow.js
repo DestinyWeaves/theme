91c750b1da601c6959e0a7024aaf19c8d8dea8c0
Anson Mansfield <anson.mansfield@gmail.com>
2024-09-07 17:29:21 -0400

    reflow the post edit box

--- assets/js/jcink-reflow.js
+++ assets/js/jcink-reflow.js
@@ -114,8 +114,23 @@ document.querySelectorAll(".jcink-reflow-googleads").forEach(div => {
     tableborder_div.previousElementSibling.remove(); // random br
     tableborder_div.previousElementSibling.remove(); // add reply / new topic / new poll
     tableborder_div.nextElementSibling.remove(); // random br
-    // tableborder_div.nextElementSibling.remove(); // add reply / fast reply / new topic / new poll
-    // tableborder_div.replaceWith(...tableborder_div.childNodes); // unwrap
-    // expMenu('qr_show'); // open the fast reply box (since we removed the link to open it manually)
-  }
+    tableborder_div.nextElementSibling.remove(); // add reply / fast reply / new topic / new poll
+    tableborder_div.replaceWith(...tableborder_div.childNodes); // unwrap
+
+    
+    const post_box = document.querySelector("#qr_open");
+    const inner_box = document.querySelector("#qr_open div.tableborder");
+    post_box.attributes.id = "";
+    post_box.attributes.align = "";
+    post_box.attributes.style = ""; // unhide the fast post box
+    inner_box.attributes.style = "display:none";
+    inner_box.className = "";
+
+    const textinput = document.querySelector('textinput[name="Post"]');
+    textinput.remove();
+    post_box.appendChild(textinput);
+
+    const submitbutton = document.querySelector('input[name="submit"]');
+    submitbutton.remove();
+    post_box.appendChild(submitbutton);
 };
